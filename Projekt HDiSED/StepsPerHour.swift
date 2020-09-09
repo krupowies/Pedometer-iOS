771af6e529fe18797e1614ceabb774f3b33e842e
Wojciech Krupowies <krupowieswojciech@gmail.com>
2020-09-09 13:57:37 +0200

    step counting func created

---
+++ Projekt HDiSED/StepsPerHour.swift
@@ -0,0 +1,18 @@
+//
+//  StepsPerHour.swift
+//  Projekt HDiSED
+//
+//  Created by Wojtek Krupowies on 09/09/2020.
+//  Copyright © 2020 Wojtek Krupowies i Mateusz Kula. All rights reserved.
+//
+
+import Foundation
+
+struct StepsPerHour {
+    let numberOfSteps: Int
+    let day: Int
+    let month: Int
+    let year: Int
+    let hour: Int
+    let minutes: Int
+}
--- Projekt HDiSED/ViewController.swift
+++ Projekt HDiSED/ViewController.swift
@@ -7,16 +7,55 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var numberOfSteps: UILabel!
     
+    let activityManager = CMMotionActivityManager()
+    let pedometer = CMPedometer()
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //repeatFunc()
+        countSteps()
     }
-
     
+    func repeatFunc(){
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.printDate), userInfo: nil, repeats: true)
+    }
+    
+    @objc func printDate(){
+        let date = Date()
+        let calendar = Calendar.current
+        let day = calendar.component(.day, from: date)
+        let month = calendar.component(.month, from: date)
+        let year = calendar.component(.year, from: date)
+        let hour = calendar.component(.hour, from: date)
+        let minutes = calendar.component(.minute, from: date)
+        let seconds = calendar.component(.second, from: date)
+        print("day : \(day) month : \(month) year : \(year)")
+        print("hour : \(hour) and minutes \(minutes)")
+        print("seconds : \(seconds)")
+    }
+    
+    func countSteps(){
+        if CMPedometer.isStepCountingAvailable() {
+            self.pedometer.startUpdates(from: Date()) { (data, error) in
+                if error == nil {
+                    if let response = data {
+                        DispatchQueue.main.async {
+                            print("STEPS : \(response.numberOfSteps)")
+                            self.numberOfSteps.text = response.numberOfSteps.stringValue
+                            print("dupa")
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
