0448ad33d78f03ca1edc705ceee10ef8b0e1ffe2
Wojciech Krupowies <krupowieswojciech@gmail.com>
2020-09-09 09:06:49 +0200

    Initial Commit

---
+++ Projekt HDiSED/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Projekt HDiSED
+//
+//  Created by Wojtek Krupowies on 09/09/2020.
+//  Copyright © 2020 Wojtek Krupowies i Mateusz Kula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
