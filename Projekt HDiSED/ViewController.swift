722ad6868bb3e02ed830ef81d07604f284a5687e
Wojciech Krupowies <krupowieswojciech@gmail.com>
2020-09-15 11:53:30 +0200

    Data successfully exported to database.

--- Projekt HDiSED/ViewController.swift
+++ Projekt HDiSED/ViewController.swift
@@ -9,11 +9,24 @@
 import UIKit
 import CoreMotion
 import Firebase
-import StepData
+
+struct StepData {
+    let numberOfSteps: Int
+    let day: Int
+    let month: Int
+    let year: Int
+    let hour: Int
+    let minutes: Int
+    let seconds: Int
+}
+
+
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var numberOfSteps: UILabel!
+    
+    let db = Firestore.firestore()
     
     let activityManager = CMMotionActivityManager()
     let pedometer = CMPedometer()
@@ -23,29 +36,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //repeatFunc()
-        countSteps()
+        
+        numberOfSteps.text = "12"
+        let testPack = createPack()
+        print(testPack)
+        sendPack(pack: testPack)
     }
     
-    func repeatFunc(){
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.printDate), userInfo: nil, repeats: true)
-    }
+//    func repeatFunc(){
+//        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.printData), userInfo: nil, repeats: true)
+//    }
     
-    @objc func printDate(){
-        let date = Date()
-        let calendar = Calendar.current
-        let day = calendar.component(.day, from: date)
-        let month = calendar.component(.month, from: date)
-        let year = calendar.component(.year, from: date)
-        let hour = calendar.component(.hour, from: date)
-        let minutes = calendar.component(.minute, from: date)
-        let seconds = calendar.component(.second, from: date)
-        // let timeInterval = NSDate().timeIntervalSince1970
-        print("day : \(day) month : \(month) year : \(year)")
-        print("hour : \(hour) and minutes \(minutes)")
-        print("seconds : \(seconds)")
-    }
     
     func countSteps() {
         if CMPedometer.isStepCountingAvailable() {
@@ -62,27 +63,38 @@ class ViewController: UIViewController {
         }
     }
     
-    func currentTime()->Calendar {
+    
+    func createPack()->StepData {
+        
         let date = Date()
         let calendar = Calendar.current
         
-        return calendar
+        if let text = self.numberOfSteps.text, let stepValue = Int(text) {
+            
+            let yearCur = calendar.component(.year, from: date)
+            let monthCur = calendar.component(.month, from: date)
+            let dayCur = calendar.component(.day, from: date)
+            let hourCur = calendar.component(.hour, from: date)
+            let minutesCur = calendar.component(.minute, from: date)
+            let secondsCur = calendar.component(.second, from: date)
+            
+            return StepData(numberOfSteps: stepValue, day: dayCur, month: monthCur, year: yearCur, hour: hourCur, minutes: minutesCur, seconds: secondsCur)
+        }
+        return StepData(numberOfSteps: 0, day: 0, month: 0, year: 0, hour: 0, minutes: 0, seconds: 0)
     }
     
-    
-    func createPack()->StepData {
-        let calendar = currentTime()
-        let stepValue = Int(numberOfSteps.text) ?? 0
-        let year = calendar.component(.year, from: date)
-        let month = calendar.component(.month, from: date)
-        let day = calendar.component(.day, from: date)
-        let hour = calendar.component(.hour, from: date)
-        let minutes = calendar.component(.minute, from: date)
-        let seconds = calendar.component(.second, from: date)
-        
-        let pack = StepData
+    func sendPack(pack: StepData) {
+        db.collection("steps").addDocument(data:
+            ["Number of steps": pack.numberOfSteps,
+             "seconds": pack.seconds
+        ]) { (error) in
+            if let e = error {
+                print("Problem found : \(e)")
+            } else {
+                print("Data saved !!")
+            }
+        }
     }
-    
     
 }
 
